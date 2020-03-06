9b0ba11c52740b98e5fcd3a3273949d1887508aa
CArlos <cgfigueroa95@gmail.com>
2020-03-06 02:11:57 -0300

    enviroment arreglado para heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDpizLoAJiAt6FzNI7dGrvAWCY4OvuJEXU",
+    authDomain: "gamestore-8c4e3.firebaseapp.com",
+    databaseURL: "https://gamestore-8c4e3.firebaseio.com",
+    projectId: "gamestore-8c4e3",
+    storageBucket: "gamestore-8c4e3.appspot.com",
+    messagingSenderId: "500094964416",
+    appId: "1:500094964416:web:faabd12047cfaa86671325"
+  }
+  
 };
